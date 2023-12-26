90ef3545aac69c3d1974f9cb112ca80ef8a873e8
unknown <chiedozieezenwa@gmail.com>
2023-12-26 23:30:53 +0100

    header section

--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -1,6 +1,18 @@
 function Header() {
   return (
-    <div>Man</div>
+    <div className="header">
+      <h1>Little Lemon</h1>
+      <h4>Chicago</h4>
+      <p>
+        We're a family owned
+        <br />
+        Nigerian Restaurant
+        <br />
+        focused on traditional recipes <br />
+        served with a modern twist
+      </p>
+      <button>Reserve a table</button>
+    </div>
   );
 }
 export default Header;
